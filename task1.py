f0cb42901d81a5b7257d7f33321594bda007315c
agzam <agzammuhammedjanov@gmail.com>
2026-03-28 22:51:40 +0500

    done

--- task1.py
+++ task1.py
@@ -1,20 +1,50 @@
 customer = input("Enter customer name: ")
 
-item = []
 count = 0
 subtotal = 0 
 
 while True:
 	item = input("Enter item name (or 'done' to finish): ")
-	count +=1
 
 	if item == "done":
 		break
+	count +=1
 
 	price = int(input("Enter price: "))
 	subtotal += price
 
-
+print("="*30)
 print("Customer: ", customer)
 print("Items: ", count)
 print("Subtotal: ", subtotal, "KZT")
+print("="*30)
+
+
+hour = int(input("Enter current hour (0-23): "))
+
+if 6 <= hour < 12:
+	period = "Morning discount"
+	discount = 0.10
+
+elif 12 <= hour < 17:
+	period = "No discount"
+	discount = 0
+
+elif 17 <= hour < 22:
+	period = "Evening discount"
+	discount = 0.05
+
+else:
+	print("Closed")
+	exit()
+
+amount = subtotal * discount
+after_dis = subtotal - amount
+tip = after_dis * 0.10
+total = after_dis + tip
+
+print("="*30)
+print("Time period :", period)
+print("Discount :", discount, "KZT")
+print("Total :", total, "KZT")
+print("="*30)
--- task3.py
+++ task3.py
@@ -1,3 +1,9 @@
-name = input("inter name: ")
+name = input("enter name: ")
 print("Name uppercase :", name.upper())
 print("Name lowercase :", name.lower())
+print("Name length :", len(name))
+
+if name[0].upper() == 'A' or name[0].upper() == 'S':
+    print("VIP customer")
+else:
+    print("Regular customer")
